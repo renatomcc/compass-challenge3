572188aeb2e064defce2ba4dca8046a3183debd8
Renato <renattocasttro@gmail.com>
2023-12-27 10:21:24 -0300

    feat: create event method created on controller

--- src/controllers/EventsController/EventsController.ts
+++ src/controllers/EventsController/EventsController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express'
+import EventsServices from '../../services/EventsServices/EventsServices'
+import IEvent from '../../interfaces/Event'
 
 export default class EventsController {
-  static async createEvent(req: Request, res: Response) {}
+  static async createEvent(req: Request, res: Response) {
+    const newEvent: IEvent = req.body;
+    const result = await EventsServices.createEvent(newEvent)
+    return res.status(200).json({ result })
+  }
   static async getAllEventsByDay(req: Request, res: Response) {}
   static async deleteEventsByDay(req: Request, res: Response) {}
   static async getEventsById(req: Request, res: Response) {}
