107e916210c9f88f8d3b49ee30c24a0d0a77d1fe
Renato <renattocasttro@gmail.com>
2023-12-27 10:30:22 -0300

    feat: post and get by day of week routes added on routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,11 +1,14 @@
 import express, { Router } from 'express'
 import { SignUpController } from '../controllers/UserController/SignUpController'
 import { SignInController } from '../controllers/UserController/SignInController'
+import EventsController from '../controllers/EventsController/EventsController'
 const Routes: Router = express.Router()
 
 const prefix = '/api/v1'
 
 Routes.post(`${prefix}/users/sign-up`, SignUpController.handle)
-      .post(`${prefix}/users/sign-in`, SignInController.handle)
+  .post(`${prefix}/users/sign-in`, SignInController.handle)
+  .post(`${prefix}/events`, EventsController.createEvent)
+  .get(`${prefix}/events/:dayOfWeek`, EventsController.getAllEventsByDay)
 
 export default Routes
