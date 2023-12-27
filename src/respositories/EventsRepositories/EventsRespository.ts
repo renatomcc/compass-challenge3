b58d1dac1136cab1aac616e1b9b13dc280eb6785
Renato <renattocasttro@gmail.com>
2023-12-27 10:18:53 -0300

    feat: create event method created on repository

--- src/respositories/EventsRepositories/EventsRespository.ts
+++ src/respositories/EventsRepositories/EventsRespository.ts
@@ -1,5 +1,11 @@
+import IEvent from '../../interfaces/Event'
+import Event from '../../model/Event'
+
 export default class EventsRepository {
-  static async createEvent(req: Request, res: Response) {}
+  static async createEvent(event: IEvent) {
+    const newEvent = await Event.create(event)
+    return newEvent
+  }
   static async getAllEventsByDay(req: Request, res: Response) {}
   static async deleteEventsByDay(req: Request, res: Response) {}
   static async getEventsById(req: Request, res: Response) {}
