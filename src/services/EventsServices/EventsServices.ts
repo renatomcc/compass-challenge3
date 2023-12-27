12770a893fdd38cf27f5788304d8f7dd431e61a8
Renato <renattocasttro@gmail.com>
2023-12-27 10:20:15 -0300

    feat: create event method created on service

--- src/services/EventsServices/EventsServices.ts
+++ src/services/EventsServices/EventsServices.ts
@@ -1,7 +1,11 @@
 import jwt from 'jsonwebtoken'
+import IEvent from '../../interfaces/Event'
+import EventsRepository from '../../respositories/EventsRepositories/EventsRespository'
 
 export default class EventsServices {
-  static async createEvent(req: Request, res: Response) {}
+  static async createEvent(payload: IEvent) {
+    return await EventsRepository.createEvent(payload)
+  }
   static async getAllEventsByDay(req: Request, res: Response) {}
   static async deleteEventsByDay(req: Request, res: Response) {}
   static async getEventsById(req: Request, res: Response) {}
