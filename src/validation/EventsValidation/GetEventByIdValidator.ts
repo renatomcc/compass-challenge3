783f3e603dce417f9ccffa69c460d0056fca0455
Renato <renattocasttro@gmail.com>
2023-12-30 01:03:32 -0300

    feat: get event by id validator created

---
+++ src/validation/EventsValidation/GetEventByIdValidator.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+import Event from '../../model/Event'
+
+interface ValidationError {
+  resource: string
+  message: string
+}
+
+export default async function GetEventByIdValidator(payload: string) {
+  if (!mongoose.Types.ObjectId.isValid(payload)) {
+    const invalidIdError: ValidationError = {
+      resource: 'id',
+      message: 'Invalid ObjectId',
+    }
+    return {
+      type: 'Validation error',
+      errors: [invalidIdError],
+      statusCode: 422,
+    }
+  }
+
+  const event = await Event.findById(payload)
+  if (!event) {
+    const notFoundError: ValidationError = {
+      resource: 'id',
+      message: 'Event not found',
+    }
+    return {
+      type: 'Validation error',
+      errors: [notFoundError],
+      statusCode: 404,
+    }
+  }
+  return { statusCode: 200 }
+}
