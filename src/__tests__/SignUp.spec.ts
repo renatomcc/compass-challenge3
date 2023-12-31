e32d8c8d4b2dc75becb30ba01c7d5dd5497eb022
Renato <renattocasttro@gmail.com>
2023-12-31 02:23:06 -0300

    build: superset and jest dependencies installed

--- src/__tests__/SignUp.spec.ts
+++ src/__tests__/SignUp.spec.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 import User from '../model/User'
 
 describe('Sign Up', () => {
-  let createdUserId: string
+  let createdUserId: string | undefined
 
   afterAll(async () => {
     if (createdUserId) {
@@ -14,5 +14,29 @@ describe('Sign Up', () => {
     mongoose.connection.close()
   })
 
-  it('should create a new user', async () => {})
+  it('should create a new user', async () => {
+    const userData = {
+      firstName: 'Shakira',
+      lastName: 'Isabel',
+      birthDate: '1977-02-02',
+      city: 'Barranquilla',
+      country: 'Colômbia',
+      email: 'shakira@wakawaka.com',
+      password: 'hipsdontlie',
+      confirmPassword: 'hipsdontlie',
+    }
+
+    const response = await request(app)
+      .post('/api/v1/users/sign-up')
+      .send(userData)
+      .set('Accept', 'application/json')
+
+    console.log(response.status)
+    console.log(response.body)
+
+    // Assuming User.findByIdAndDelete sets createdUserId when successful
+    createdUserId = response.body.id // Adjust this line based on your actual logic
+
+    expect(response.status).toBe(200)
+  })
 })
