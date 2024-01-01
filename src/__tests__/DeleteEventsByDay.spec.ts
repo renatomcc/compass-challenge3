26c6cb9598c0cb41c2a1614a5e8e92e96e569108
Renato <renattocasttro@gmail.com>
2024-01-01 04:38:12 -0300

    feat: delete events by day test added

---
+++ src/__tests__/DeleteEventsByDay.spec.ts
@@ -0,0 +1,88 @@
+import request from 'supertest'
+import app from '../app'
+import mongoose from 'mongoose'
+import User from '../model/User'
+
+describe('Delete Events by day', () => {
+  let createdUserId: string
+
+  afterAll(async () => {
+    if (createdUserId) {
+      await User.findByIdAndDelete(createdUserId)
+    }
+  })
+  it('should delete and return all events on a specific day of the week', async () => {
+    const userData = {
+      firstName: 'Shakira',
+      lastName: 'Isabel',
+      birthDate: '1977-02-02',
+      city: 'Barranquilla',
+      country: 'Colômbia',
+      email: 'shakira@wakawaka.com',
+      password: 'hipsdontlie',
+      confirmPassword: 'hipsdontlie',
+    }
+
+    const userLogin = {
+      email: 'shakira@wakawaka.com',
+      password: 'hipsdontlie',
+    }
+
+    const signUpResponse = await request(app)
+      .post('/api/v1/users/sign-up')
+      .send(userData)
+      .set('Accept', 'application/json')
+
+    createdUserId = signUpResponse.body._id
+
+    const signInResponse = await request(app)
+      .post('/api/v1/users/sign-in')
+      .send(userLogin)
+      .set('Accept', 'application/json')
+
+    const token = signInResponse.body.token
+
+    const eventData = {
+      description: 'Show',
+      dayOfWeek: 'sunday',
+    }
+
+    const eventData2 = {
+      description: 'Show2',
+      dayOfWeek: 'sunday',
+    }
+
+    const eventData3 = {
+      description: 'Show3',
+      dayOfWeek: 'sunday',
+    }
+
+    await request(app)
+      .post('/api/v1/events')
+      .send(eventData)
+      .set('Authorization', `Bearer ${token}`)
+
+    await request(app)
+      .post('/api/v1/events')
+      .send(eventData2)
+      .set('Authorization', `Bearer ${token}`)
+
+    await request(app)
+      .post('/api/v1/events')
+      .send(eventData3)
+      .set('Authorization', `Bearer ${token}`)
+
+    const dayOfWeek: string = 'sunday'
+
+    const deleteEventsResponse = await request(app)
+      .delete(`/api/v1/events?dayOfWeek=${dayOfWeek}`)
+      .set('Authorization', `Bearer ${token}`)
+
+    console.log(deleteEventsResponse.body)
+    expect(deleteEventsResponse.status).toBe(200)
+    // expect(deleteEventsResponse.body).toBeDefined
+    // expect(deleteEventsResponse.body._id).toBeDefined
+    // expect(deleteEventsResponse.body.description).toBeDefined
+    // expect(deleteEventsResponse.body.userId).toBeDefined
+  })
+})
