a7615a318b6935271416e12fcdb6063a1a47249d
Renato <renattocasttro@gmail.com>
2024-01-02 18:40:40 -0300

    refactor: test if password is changed removed from User model

--- src/model/User.ts
+++ src/model/User.ts
@@ -13,9 +13,6 @@ const userSchema = new Schema({
 })
 
 userSchema.pre('save', async function () {
-  if (!this.isModified('password')) {
-    return
-  }
   const salt = await bcrypt.genSalt(12)
   this.password = await bcrypt.hash(this.password!, salt)
   this.confirmPassword = this.password
