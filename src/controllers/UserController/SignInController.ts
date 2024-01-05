8b599c7e577a28133a5888c0954d1eeacba04b2f
Renato <renattocasttro@gmail.com>
2024-01-05 04:21:00 -0300

    refactor: errorHandler added on signup and signin controllers

--- src/controllers/UserController/SignInController.ts
+++ src/controllers/UserController/SignInController.ts
@@ -1,22 +1,17 @@
-import { Request, Response } from 'express';
-import ISignInUser from '../../interfaces/SignIn';
-import SignInService from '../../services/UserServices/SignInService';
-import CustomError from '../../errors/CustomError';
+import { Request, Response } from 'express'
+import ISignInUser from '../../interfaces/SignIn'
+import SignInService from '../../services/UserServices/SignInService'
+import CustomError from '../../errors/CustomError'
+import { handleError } from '../../utils/errorHandler'
 
 export default class SignInController {
   static async handle(req: Request, res: Response) {
     try {
-      const userLogin: ISignInUser = req.body;
-      const result = await SignInService.execute(userLogin);
-      return res.status(200).json(result);
+      const userLogin: ISignInUser = req.body
+      const result = await SignInService.execute(userLogin)
+      return res.status(200).json(result)
     } catch (err) {
-      if (err instanceof CustomError) {
-        return res
-          .status(err.statusCode)
-          .json({ type: err.type, errors: err.errors });
-      } else {
-        return res.status(500).json({ message: 'Internal Server Error' });
-      }
+      return handleError(err, res)
     }
   }
 }
--- src/controllers/UserController/SignUpController.ts
+++ src/controllers/UserController/SignUpController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import CustomError from '../../errors/CustomError'
 import ISignUpUser from '../../interfaces/SignUp'
 import SignUpService from '../../services/UserServices/SignUpService'
+import { handleError } from '../../utils/errorHandler'
 
 export default class SignUpController {
   static async handle(req: Request, res: Response) {
@@ -10,13 +11,7 @@ export default class SignUpController {
       const newUser = await SignUpService.execute(payload)
       return res.status(200).json(newUser)
     } catch (err) {
-      if (err instanceof CustomError) {
-        return res
-          .status(err.statusCode)
-          .json({ type: err.type, errors: err.errors })
-      } else {
-        return res.status(500).json({ message: 'Internal Server Error' })
-      }
+      return handleError(err, res)
     }
   }
 }
